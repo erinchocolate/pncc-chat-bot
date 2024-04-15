dc6763ddd59b814b889b597a90c5e7fb3c28b0d5
meiqiao <chenmeiqiao940926@gmail.com>
2024-04-15 20:52:35 +1200

    use llamaindex to parse pdf

--- app.py
+++ app.py
@@ -1,23 +1,55 @@
 import os
-import subprocess
 from dotenv import load_dotenv
-from llama_index.llama_pack import download_llama_pack
+from llama_parse import LlamaParse
+from llama_index.core import Settings
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.core.node_parser import MarkdownElementNodeParser
+from llama_index.core import VectorStoreIndex
+from llama_index.postprocessor.flag_embedding_reranker import FlagEmbeddingReranker
 
 load_dotenv()
 
-api_key = os.getenv("OPENAI_API_KEY")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+llama_api_key = os.getenv("LLAMA_API_KEY")
 
-EmbeddedTablesUnstructuredRetrieverPack = download_llama_pack(
-    "EmbeddedTablesUnstructuredRetrieverPack",
-    "./embedded_tables_unstructured_pack",
+def parse_pdf(file_name):
+    parser = LlamaParse(
+        api_key=llama_api_key,
+        result_type="markdown",
+        verbose=True,
+        language="en",
+        num_workers=2,
+    )
+
+    documents = parser.load_data(file_name)
+    return documents
+
+documents = parse_pdf("section-1-general-introduction-v4.pdf")
+
+Settings.llm = OpenAI(model="gpt-3.5-turbo")
+Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small")
+
+node_parser = MarkdownElementNodeParser(llm=OpenAI(model="gpt-3.5-turbo"), num_workers=8)
+
+nodes = node_parser.get_nodes_from_documents(documents=[documents[0]])
+
+base_nodes, objects = node_parser.get_nodes_and_objects(nodes)
+
+recursive_index = VectorStoreIndex(nodes=base_nodes+objects)
+
+reranker = FlagEmbeddingReranker(
+    top_n=5,
+    model="BAAI/bge-reranker-large",
 )
 
-def convert_pdf_to_html(pdf_path, html_path):
-    command = f"pdf2htmlEX {pdf_path} --dest-dir {html_path}"
-    subprocess.call(command, shell=True)
+recursive_query_engine = recursive_index.as_query_engine(
+    similarity_top_k=15,
+    node_postprocessors=[reranker],
+    verbose=True
+)
 
-input_pdf = "quarterly-nvidia.pdf"
-output_pdf = "quarterly-nvidia"
+query = "How many hectares does residential zone have?"
+response = recursive_query_engine.query(query)
 
-convert_pdf_to_html(input_pdf, output_pdf)
-
+print(response)
